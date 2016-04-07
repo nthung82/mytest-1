57a060320736a6efbf1d261bdb398a61b49a6b7e
hungntvnext <hungnt@vnext.vn>
2016-04-07 15:00:41 +0700

    Update Day2Branch.java

--- src/main/java/com/mkyong/web/controller/Day2Branch.java
+++ src/main/java/com/mkyong/web/controller/Day2Branch.java
@@ -2,6 +2,9 @@ package com.mkyong.web.controller;
 
 public class Day2Branch {
 private void test(){
-  System.out.print();
+  System.out.print("111111");
+}
+private void test2(){
+  System.out.print("111111");
 }
 }
