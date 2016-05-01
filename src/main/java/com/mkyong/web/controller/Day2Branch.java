a7475f3f324153fa796ff1bae0fef6a55558392f
Nguyen Thanh Hung <nthung82@gmail.com>
2016-05-01 21:06:01 +0700

    'aaaaaa'

--- src/main/java/com/mkyong/web/controller/Day2Branch.java
+++ src/main/java/com/mkyong/web/controller/Day2Branch.java
@@ -3,7 +3,7 @@ package com.mkyong.web.controller;
 public class Day2Branch {
 private void test(){
 
-  System.out.print("111111xxxxxxxxxxxxxxxx111111111111111czczxczxc11111");
+  System.out.print("111111xxxxxxxxxx1xxxxxx111111111111111czczxczxc11111");
 
   System.out.print("111111");
 }
