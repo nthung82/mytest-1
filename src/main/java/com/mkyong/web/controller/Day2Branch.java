397966a31bb17ac9780941846bb7f563eab1cb6e
Nguyen Thanh Hung <nthung82@gmail.com>
2016-05-09 23:06:55 +0700

    adasda

--- src/main/java/com/mkyong/web/controller/Day2Branch.java
+++ src/main/java/com/mkyong/web/controller/Day2Branch.java
@@ -3,12 +3,13 @@ package com.mkyong.web.controller;
 public class Day2Branch {
 private void test(){
 
-  System.out.print("111111xxxxxxxxxx1xxxxxx111111111111111czczxczxc11111");
+  System.out.print("my badness_new1");
 
-  System.out.print("111111");
+  System.out.print("newsssss");
+  System.out.print("sdfsfsdfs moi commit sdfsfs");
 }
 private void test2(){
   System.out.print("111111");
-  System.out.print("111111 was mersssssssssssssssssssger when conflict");
+  System.out.print("sfdsfsdfsdf  was  when conflict");
 }
 }
