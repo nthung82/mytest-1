a45bf2f9880f9fcf9abff1887c2917cf32897884
Nguyen Thanh Hung <nthung82@gmail.com>
2016-05-14 10:09:12 +0700

    a

--- src/main/java/com/mkyong/web/controller/Day2Branch.java
+++ src/main/java/com/mkyong/web/controller/Day2Branch.java
@@ -2,18 +2,7 @@ package com.mkyong.web.controller;
 
 public class Day2Branch {
 private void test(){
-
-<<<<<<< HEAD
-  System.out.print("my commit2");
-=======
-  System.out.print("my badness_new111111111111");
-<<<<<<< HEAD1111111111111111
-
-=======
->>>>>>> 2544a7d4cc4f27996f4e377ed435c0c599fd3688
->>>>>>> db03c97b96c4c7b4206d5251b350ea159555cb07
-
-  System.out.print("newsssss");
+  System.out.print("newsssss_New check out");
   System.out.print("sdfsfsdfs moi commit sdfsfs");
 }
 private void test2(){
